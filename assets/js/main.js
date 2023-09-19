312b0e40f0bc802077570de1e8228e7e8cf7afa7
aptitudepi <45304559+aptitudepi@users.noreply.github.com>
2023-09-18 22:38:10 -0500

    Banerjee, Devkumar - Fix JS Animations after Window Loads

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-(function ($) {
+window.onload = (function ($) {
   $('#globe').css({marginTop: '-100px'});
   const world = Globe()(document.getElementById("globe"))
     .backgroundColor("rgba(0,0,0,0)")
