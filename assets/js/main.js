f190f899df00d8b6f046a26e0e263b392ccd9e01
Devkumar Banerjee <45304559+aptitudepi@users.noreply.github.com>
2024-02-02 13:40:31 -0600

    Revert main.js to known good

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,8 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+import THREE from './three.module.js';
+
 (function ($) {
   // Three.js WebGL Globe
   $("#globe").css({ marginTop: "-200px" });
